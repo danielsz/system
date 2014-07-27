1d67caafe0002810e830f7ee9b328228efd9eb14
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2014-07-28 02:59:26 +0300

    sente 0.1.5

--- src/framework/components/sente.clj
+++ src/framework/components/sente.clj
@@ -2,16 +2,17 @@
   (:require [com.stuartsierra.component :as component]
             [taoensso.sente :as sente]))
 
-(defrecord ChannelSockets [ring-ajax-post ring-ajax-get-or-ws-handshake ch-chsk chsk-send! chsk-router handler]
+(defrecord ChannelSockets [ring-ajax-post ring-ajax-get-or-ws-handshake ch-chsk chsk-send! connected-uids chsk-router handler]
   component/Lifecycle
   (start [component]
-    (let [{:keys [ch-recv send-fn ajax-post-fn ajax-get-or-ws-handshake-fn]}
+    (let [{:keys [ch-recv send-fn ajax-post-fn ajax-get-or-ws-handshake-fn connected-uids]}
       (sente/make-channel-socket! {})]
       (assoc component 
         :ring-ajax-post ajax-post-fn 
         :ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn
         :ch-chsk ch-recv
         :chsk-send! send-fn
+        :connected-uids connected-uids
         :chsk-router (sente/start-chsk-router-loop! handler ch-recv))))
   (stop [component]
     ((:chsk-router component))
