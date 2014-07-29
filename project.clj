f05dc7f9aabea1584e3175ee340770b6683e9bbc
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2014-07-29 08:30:37 +0300

    dev dependencies

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring "1.3.0"]
-                 [org.clojure/tools.nrepl "0.2.3"]
                  [reloaded.repl "0.1.0"]
-                 [com.stuartsierra/component "0.2.1"]
-                 [com.datomic/datomic-free "0.9.4815.12"]
-                 [com.novemberain/monger "2.0.0"]
-                 [com.novemberain/langohr "2.11.0"]
-                 [com.taoensso/sente "0.15.1"]
-                 [http-kit "2.1.18"]]
+                 [com.stuartsierra/component "0.2.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.3"]
+                                  [ring "1.3.0"]
+                                  [com.datomic/datomic-free "0.9.4815.12"]
+                                  [com.novemberain/monger "2.0.0"]
+                                  [com.novemberain/langohr "2.11.0"]
+                                  [com.taoensso/sente "0.15.1"]
+                                  [http-kit "2.1.18"]]}}
   :scm {:name "git"
         :url "https://github.com/danielsz/system"})
