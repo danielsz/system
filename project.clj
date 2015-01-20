55e81dc6b9f56ff222bb8aec3d4989929112c08f
Dariusz Luksza <dariusz@luksza.org>
2015-01-20 23:35:25 +0100

    Bump version of sente and monger

--- project.clj
+++ project.clj
@@ -10,11 +10,11 @@
                                   [ring "1.3.1"]
                                   [im.chit/hara.io.watch "2.1.7"]
                                   [com.datomic/datomic-free "0.9.4815.12"]
-                                  [com.novemberain/monger "2.0.0"]
+                                  [com.novemberain/monger "2.0.1"]
                                   [org.clojure/java.jdbc "0.3.5"]
                                   [com.h2database/h2 "1.4.181"]
                                   [com.novemberain/langohr "2.11.0"]
-                                  [com.taoensso/sente "1.0.0"]
+                                  [com.taoensso/sente "1.3.0"]
                                   [org.danielsz/etsy "0.1.2" ]
                                   [http-kit "2.1.19"]
                                   [aleph "0.4.0-alpha9"]]}}
