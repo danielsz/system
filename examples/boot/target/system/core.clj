7229442b397e827ad8fc54f37e5e577290b22aaf
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2015-04-06 17:48:30 +0300

    cleaned up target directories

--- examples/boot/target/system/core.clj
+++
@@ -1,8 +0,0 @@
-(ns system.core
-  (:require 
-   [com.stuartsierra.component :as component]))
-
-(defmacro defsystem
-  "Convenience macro to build a system"
-  [fname system-map]
-  `(defn ~fname [] (component/system-map ~@system-map)))
