9a94000feee8bc58609f3b43ec377e90c2e51980
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2015-08-15 22:43:35 +0200

    preparing for 0.2.0 (snapshot)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.danielsz/system "0.1.9"
+(defproject org.danielsz/system "0.2.0-SNAPSHOT"
   :description "Reloaded components à la carte"
   :url "https://github.com/danielsz/system"
   :license {:name "Eclipse Public License"
