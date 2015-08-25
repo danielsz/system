1bdc182022bdbd79129d4f75bf770613b60a6499
Larry Staton Jr <larry@larrystaton.com>
2015-08-25 12:33:13 +0000

    Add component for Immutant web server.

---
+++ test/system/components/immutant_web_test.clj
@@ -0,0 +1,16 @@
+(ns system.components.immutant-web-test
+  (:require [system.components.immutant-web :refer [new-web-server]]
+   [com.stuartsierra.component :as component]
+   [clojure.test :refer [testing deftest is]]))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
+
+(def http-server (new-web-server 8081 handler))
+
+(deftest http-server-lifecycle
+  (alter-var-root #'http-server component/start)
+  (is (:server http-server) "HTTP server has been added to component")
+  (alter-var-root #'http-server component/stop))
