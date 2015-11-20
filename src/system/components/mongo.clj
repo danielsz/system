0a7c77b8aa42cb9b998a3f22eb530d29c0e3992e
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2015-11-20 06:12:46 +0200

    mongo component supports init-fn (for indices, for example)

--- src/system/components/mongo.clj
+++ src/system/components/mongo.clj
@@ -2,14 +2,16 @@
   (:require [com.stuartsierra.component :as component]
             [monger.core :as mg]))
 
-(defrecord Mongo [uri db]
+(defrecord Mongo [uri db init-fn]
   component/Lifecycle
   (start [component]
     (if uri 
-      (let [{:keys [conn db]} (mg/connect-via-uri uri)]
+      (let [{:keys [conn db]} (mg/connect-via-uri uri)
+            _ (when init-fn (init-fn db))]
         (assoc component :db db))
-      (let [conn (mg/connect)]
-        (assoc component :db (mg/get-db conn "mongo-dev")))))
+      (let [conn (mg/connect)
+            db (mg/get-db conn "mongo-dev")]
+        (assoc component :db db))))
   (stop [component]
     (assoc component :db nil)))
 
@@ -17,7 +19,9 @@
   ([]
      (map->Mongo {}))
   ([uri]
-     (map->Mongo {:uri uri})))
+   (map->Mongo {:uri uri}))
+  ([uri init-fn]
+     (map->Mongo {:uri uri :init-fn init-fn})))
 
 
 
--- test/system/components/mongo_test.clj
+++ test/system/components/mongo_test.clj
@@ -9,6 +9,8 @@
 
 (def mongo-db-prod (new-mongo-db "mongodb://127.0.0.1/monger-test4"))
 (def mongo-db-dev (new-mongo-db)) 
+(def mongo-with-indices (new-mongo-db "mongodb://127.0.0.1/monger-test4" #(do (monger.collection/ensure-index % "coll" (array-map :shop-id 1))
+                                                                              (monger.collection/ensure-index % "users" {:email 1} {:unique true}))))
 
 (deftest mongo-production
   (alter-var-root #'mongo-db-prod component/start)
@@ -25,3 +27,14 @@
   (monger.collection/drop (:db mongo-db-dev) "coll")
   (alter-var-root #'mongo-db-dev component/stop)
   (is (nil? (:db mongo-db-dev)) "DB is stopped"))
+
+(deftest mongo-indices
+  (alter-var-root #'mongo-with-indices component/start)
+  (is (:db mongo-with-indices) "DB has been added to component")
+  (is (= clojure.lang.PersistentHashSet (type (db/get-collection-names (:db mongo-with-indices)))) "Collections on DB is a set")
+  (is (monger.collection/exists? (:db mongo-with-indices) "users"))
+  (is (monger.collection/exists? (:db mongo-with-indices) "coll"))
+  (is (> (count (monger.collection/indexes-on (:db mongo-with-indices) "users")) 1))
+  (is (> (count (monger.collection/indexes-on (:db mongo-with-indices) "coll")) 1))
+  (alter-var-root #'mongo-with-indices component/stop)
+  (is (nil? (:db mongo-with-indices)) "DB is stopped"))
