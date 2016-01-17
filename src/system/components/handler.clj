4204363546ec1b7bc7eaa6d6be8e9767f2579838
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2016-01-17 05:39:44 +0200

    Refactoring
    * Separate middleware in its own component

--- src/system/components/handler.clj
+++ src/system/components/handler.clj
@@ -2,32 +2,15 @@
   (:require [com.stuartsierra.component :as component]
              [compojure.core :as compojure]))
 
-(defn- middleware-fn [middleware entry]
-  (if (vector? entry)
-    (let [[f & keys] entry
-          arguments  (map #(get middleware %) keys)]
-      #(apply f % arguments))
-    entry))
-
-(defn- compose-middleware [middleware]
-  (let [entries (:middleware middleware)]
-    (->> (reverse entries)
-         (map #(middleware-fn middleware %))
-         (apply comp identity))))
-
-(defrecord Handler [middleware]
+(defrecord Handler []
   component/Lifecycle
   (start [component]
-    (let [routes (keep :routes (vals component))]
-      (if middleware
-        (let [wrap-mw (compose-middleware middleware)
-              handler (wrap-mw (apply compojure/routes routes))]
-          (assoc component :handler handler))
-        (assoc component :handler (apply compojure/routes routes)))))
+    (let [routes (keep :routes (vals component))
+          wrap-mw (get-in component [:middleware :wrap-wm] identity)
+          handler (wrap-mw (apply compojure/routes routes))]
+      (assoc component :handler handler)))
   (stop [component]
     (dissoc component :handler)))
 
 (defn new-handler
-  ([] (new-handler nil))
-  ([middleware]
-   (->Handler middleware)))
+  ([] (->Handler)))
---
+++ src/system/components/middleware.clj
@@ -0,0 +1,27 @@
+(ns system.components.middleware
+  (:require [com.stuartsierra.component :as component]
+            [compojure.core :as compojure]))
+
+(defn- middleware-fn [middleware entry]
+  (if (vector? entry)
+    (let [[f & keys] entry
+          arguments  (map #(get middleware %) keys)]
+      #(apply f % arguments))
+    entry))
+
+(defn- compose-middleware [middleware]
+  (let [entries (:middleware middleware)]
+    (->> (reverse entries)
+         (map #(middleware-fn middleware %))
+         (apply comp identity))))
+
+(defrecord Middleware [middleware]
+  component/Lifecycle
+  (start [component]
+    (let [wrap-mw (compose-middleware middleware)]
+      (assoc component :wrap-wm wrap-mw)))
+  (stop [component]
+    (dissoc component :wrap-wm)))
+
+(defn new-middleware
+  ([middleware] (->Middleware middleware)))
