19925c3aafc45abb749faa925d498a71043d0a4c
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2016-01-31 04:50:04 +0200

    * Leveraging tools.namespace for hot-reloading

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [reloaded.repl "0.1.0"]
-                 [ns-tracker "0.3.0"]
+                 [org.clojure/tools.namespace "0.3.0-alpha3"]
                  [com.stuartsierra/component "0.2.3"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.11"]
                                   [cider/cider-nrepl "0.10.1"]
--- src/system/boot.clj
+++ src/system/boot.clj
@@ -1,43 +1,25 @@
 (ns system.boot
   {:boot/export-tasks true}
-  (:require [ns-tracker.core :refer :all]
-            [reloaded.repl :refer [set-init! reset go]]
+  (:require [clojure.tools.namespace.repl :as repl]
+            [reloaded.repl :refer [set-init! go]]
             [boot.core       :as core]
             [boot.util       :as util]))
 
-
-(defn- modified-files? [before-fileset after-fileset files]
-  (->> (core/fileset-diff @before-fileset after-fileset)
-       core/input-files
-       (core/by-name files)
-       not-empty))
-
 (core/deftask system [s sys SYS code "The system var."
-                      a auto-start bool "Auto-starts the system."
-                      r hot-reload bool "Enables hot-reloading."
-                      f files FILES [str] "A vector of filenames. Restricts hot-reloading to that set."]
-  (let [fs-prev-state (atom nil)
-        dirs (core/get-env :directories)
-        modified-namespaces (ns-tracker (into [] dirs))
-        auto-start (delay
-                     (when auto-start
-                       (util/info (str "Autostarting the system: " (go) "\n"))))]
+                      a auto-start bool "Auto-starts the system."]
+  (let [dirs (core/get-env :directories)
+        _ (apply repl/set-refresh-dirs dirs)]
     (fn [next-task]
       (fn [fileset]
         (#'clojure.core/load-data-readers)
         (with-bindings {#'*data-readers* (.getRawRoot #'*data-readers*)}
           (set-init! sys)
           (util/info (str "Current system: " sys "\n"))
-          @auto-start
-          (when-let [modified (modified-namespaces)]
-            (doseq [ns-sym modified]
-              (require ns-sym :reload))
-            (util/info (str "Reloading namespaces " (pr-str modified) "\n"))
-            (when hot-reload (with-bindings {#'*ns* *ns*} ; because of exception "Can't set!: *ns* from non-binding thread"
-                               (if files
-                                 (when (modified-files? fs-prev-state fileset files) (reset))
-                                 (reset)))))
-          (next-task (reset! fs-prev-state fileset)))))))
+          (when auto-start
+            (util/info (str "Autostarting the system: " (go) "\n")))
+          (with-bindings {#'*ns* *ns*} ; because of exception "Can't set!: *ns* from non-binding thread"
+            (repl/refresh))
+          (next-task fileset))))))
 
 (core/deftask run
   "Run the -main function in some namespace with arguments."
