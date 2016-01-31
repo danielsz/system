1496855bc490ae17b53e521f46f3fec9462ee866
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2016-01-31 07:07:48 +0200

    Refactoring

--- src/system/boot.clj
+++ src/system/boot.clj
@@ -1,25 +1,37 @@
 (ns system.boot
   {:boot/export-tasks true}
   (:require [clojure.tools.namespace.repl :as repl]
-            [reloaded.repl :refer [set-init! go]]
+            [reloaded.repl :refer [set-init! reset go]]
             [boot.core       :as core]
             [boot.util       :as util]))
 
+(defn- modified-files? [before-fileset after-fileset files]
+  (when files (->> (core/fileset-diff @before-fileset after-fileset)
+                core/input-files
+                (core/by-name files)
+                not-empty)))
+
 (core/deftask system [s sys SYS code "The system var."
-                      a auto-start bool "Auto-starts the system."]
-  (let [dirs (core/get-env :directories)
-        _ (apply repl/set-refresh-dirs dirs)]
+                      a auto-start bool "Auto-starts the system."
+                      f files FILES [str] "Will reset the system if a filename in that vector changes."]
+  (util/info (str "System: " sys "\n"))
+  (#'clojure.core/load-data-readers)
+  (->> (core/get-env :directories)
+    (apply repl/set-refresh-dirs))
+  (let [fs-prev-state (atom nil)
+        init-system (delay
+                      (when auto-start
+                        (util/info (str "Autostarting the system: " (go) "\n"))))]
     (fn [next-task]
       (fn [fileset]
-        (#'clojure.core/load-data-readers)
         (with-bindings {#'*data-readers* (.getRawRoot #'*data-readers*)}
           (set-init! sys)
-          (util/info (str "Current system: " sys "\n"))
-          (when auto-start
-            (util/info (str "Autostarting the system: " (go) "\n")))
+          @init-system
           (with-bindings {#'*ns* *ns*} ; because of exception "Can't set!: *ns* from non-binding thread"
-            (repl/refresh))
-          (next-task fileset))))))
+            (if (modified-files? fs-prev-state fileset files)
+              (reset)
+              (repl/refresh)))
+          (next-task (reset! fs-prev-state fileset)))))))
 
 (core/deftask run
   "Run the -main function in some namespace with arguments."
