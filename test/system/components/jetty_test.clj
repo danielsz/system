fa977e8494cb5db61fcae177c9868be8ca16bbde
brabster <paul.brabban@gmail.com>
2016-02-17 13:27:40 +0000

    Added validation for jetty component, minimal replace of existing assertions.

--- test/system/components/jetty_test.clj
+++ test/system/components/jetty_test.clj
@@ -17,4 +17,31 @@
   (alter-var-root #'http-server component/stop)
   (is (.isStopped (:server http-server)) "HTTP server stops"))
 
+(deftest http-server-valid-options-does-not-throw
+  (is (new-web-server 8081 handler {:host "example.com"
+                                    :configurator {}
+                                    :join? false
+                                    :daemon? true
+                                    :ssl-port 8443
+                                    :keystore "/tmp/keystore"
+                                    :key-password "keeping"
+                                    :truststore "/tmp/truststore"
+                                    :trust-password "secrets"
+                                    :max-threads 16
+                                    :min-threads 2
+                                    :max-idle-time 12345
+                                    :client-auth {}
+                                    :send-date-header? true
+                                    :output-buffer-size 12345
+                                    :request-header-size 12345
+                                    :response-header-size 12345})))
 
+
+(deftest http-server-port-too-low-throws
+  (is (thrown? RuntimeException (new-web-server -1 handler))))
+
+(deftest http-server-port-too-high-throws
+  (is (thrown? RuntimeException (new-web-server 65536 handler))))
+
+(deftest http-server-max-threads-too-low-throws
+  (is (thrown? RuntimeException (new-web-server 8080 handler {:max-threads 0}))))
