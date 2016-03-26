da36ca27697e13ae9e23ba163e81c878f0a5c5c6
Andrew Phillips <theasp@gmail.com>
2016-03-26 20:47:02 -0300

    Use sente 1.8.1

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                                   [com.novemberain/langohr "2.11.0"]
                                   [clojurewerkz/neocons "3.0.0"]
                                   [clojurewerkz/quartzite "2.0.0"]
-                                  [com.taoensso/sente "1.6.0"]
+                                  [com.taoensso/sente "1.8.1"]
                                   [org.danielsz/etsy "0.1.2" ]
                                   [compojure "1.4.0"]
                                   [http-kit "2.1.19"]
