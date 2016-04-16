35c290246ab44fea0d239f7031ab57d6fa9bd96f
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2016-04-16 14:11:31 +0300

    removed util namespace

--- src/system/util.clj
+++
@@ -1,7 +0,0 @@
-(ns system.util)
-
-(defn assert-options! [component-name options allowed-opts]
-  (let [invalid-keys (keys (apply dissoc options allowed-opts))]
-    (assert (not invalid-keys)
-            (format "Invalid option(s) for %s: %s"
-                    component-name (pr-str invalid-keys)))))
