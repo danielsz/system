b7f198a982077ea2cf8a3dd82f4067937d2e754b
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2016-06-15 05:18:47 +0300

    H2 fix

--- test/system/components/h2_test.clj
+++ test/system/components/h2_test.clj
@@ -1,11 +1,13 @@
 (ns system.components.h2-test
   (:use clojure.test)
-  (:require [system.components.h2 :as h2]
-            [clojure.java.jdbc :as jdbc]
-            [com.stuartsierra.component :as component]))
+  (:require
+   [system.components.h2 :as h2]
+   [system.common.h2 :as spec]
+   [clojure.java.jdbc :as jdbc]
+   [com.stuartsierra.component :as component]))
 
 (deftest test-mem-h2
-  (let [db (h2/new-h2-database h2/DEFAULT-MEM-SPEC)
+  (let [db (h2/new-h2-database spec/DEFAULT-MEM-SPEC)
         db (component/start db)]
     (jdbc/execute! db ["CREATE TABLE Temp (id int, name varchar);"])
     (jdbc/insert! db "Temp" {:id 1 :name "Bob"})
