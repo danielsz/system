881a22eb93a55b033f349ea60dad72c25083cffb
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2017-01-07 00:19:01 +0200

    konserve
    - db creation wrapped in blocking take

--- src/system/components/konserve.clj
+++ src/system/components/konserve.clj
@@ -1,14 +1,15 @@
 (ns system.components.konserve
   (:require [com.stuartsierra.component :as component]
             [konserve.filestore :refer [new-fs-store]]
-            [konserve.memory :refer [new-mem-store]]))
+            [konserve.memory :refer [new-mem-store]]
+            [clojure.core.async :as async :refer [<!!]]))
 
 (defrecord Konserve [type opts]
   component/Lifecycle
   (start [component]
     (let [store (case type
-                  :filestore (new-fs-store (:path opts))
-                  :memstore (new-mem-store))]
+                  :filestore (<!! (new-fs-store (:path opts)))
+                  :memstore (<!! (new-mem-store)))]
       (assoc component :store store)))
   (stop [component]
     (dissoc component :store)))
