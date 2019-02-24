c0caa53326f1c51f061c8c175728c36015437e9b
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2019-02-24 18:17:52 +0700

    Scheduled executor service

--- src/system/components/scheduled_executor_service.clj
+++ src/system/components/scheduled_executor_service.clj
@@ -3,18 +3,18 @@
    [com.stuartsierra.component :as component])
   (:import [java.util.concurrent ScheduledThreadPoolExecutor]))
 
-
-(defrecord Scheduler [scheduler n-threads]
+(defrecord Scheduler [n-threads xs]
   component/Lifecycle
   (start [component]
     (let [s (ScheduledThreadPoolExecutor. n-threads)]
+      (doseq [x xs]
+        (case (:method x)
+          :fixed-delay (.scheduleWithFixedDelay ^ScheduledThreadPoolExecutor s ((:f x) component) (:initial-delay x) (:period x) (:unit x))
+          :fixed-rate (.scheduleAtFixedRate ^ScheduledThreadPoolExecutor s ((:f x) component) (:initial-delay x) (:period x) (:unit x))))
       (assoc component :scheduler s)))
   (stop [component]
-    (.shutdown scheduler)
+    (.shutdown (:scheduler component))
     component))
-  
-(defn new-scheduler
-  ([]
-   (new-scheduler (.availableProcessors (Runtime/getRuntime))))
-  ([n-threads]
-   (map->Scheduler {:n-threads n-threads})))
+
+(defn new-scheduler [& {:keys [n-threads xs] :or {n-threads (.availableProcessors (Runtime/getRuntime))}}]
+  (map->Scheduler {:n-threads n-threads :xs xs}))
--- test/system/components/scheduled_executor_service_test.clj
+++ test/system/components/scheduled_executor_service_test.clj
@@ -4,15 +4,25 @@
    [system.components.scheduled-executor-service :refer [new-scheduler]]
    (system.monitoring scheduled-executor-service
                       [core :refer [started? stopped?]])
-   [clojure.test :refer [deftest is]]))
+   [clojure.test :refer [deftest is]])
+  (:import [java.util.concurrent TimeUnit]))
 
-(def scheduler (new-scheduler (+ 2 (.availableProcessors (Runtime/getRuntime)))))
+(def workers [{:f (fn [component] #(println "Hello, World. " (:n-threads component))) :initial-delay 0 :period 1 :unit TimeUnit/MINUTES :method :fixed-rate}
+              {:f (fn [component] #(println "Hello, Worlds. " (:n-threads component))) :initial-delay 0 :period 1 :unit TimeUnit/MINUTES :method :fixed-delay}])
+(def scheduler (new-scheduler :xs workers))
+(def scheduler-with-n-threads (new-scheduler :xs workers :n-threads (+ 2 (.availableProcessors (Runtime/getRuntime)))))
 
 (deftest scheduled-executor-service-test
   (alter-var-root #'scheduler component/start)
   (is (= java.util.concurrent.ScheduledThreadPoolExecutor (type (:scheduler scheduler))) "the scheduler is running")
   (alter-var-root #'scheduler component/stop)
   (is (.isShutdown (:scheduler scheduler)) "the scheduler is stopped"))
+
+(deftest scheduled-executor-service-with-threads-test
+  (alter-var-root #'scheduler-with-n-threads component/start)
+  (is (= java.util.concurrent.ScheduledThreadPoolExecutor (type (:scheduler scheduler-with-n-threads))) "the scheduler is running")
+  (alter-var-root #'scheduler-with-n-threads component/stop)
+  (is (.isShutdown (:scheduler scheduler-with-n-threads)) "the scheduler is stopped"))
 
 (deftest scheduler-monitoring-status
   (alter-var-root #'scheduler component/start)
