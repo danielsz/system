9988bbae01f25ab74f934d8882fa0d30869af8c5
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2019-05-28 01:03:02 +0300

    Remove comp alias

--- src/system/components/kampbell.clj
+++ src/system/components/kampbell.clj
@@ -1,6 +1,6 @@
 (ns system.components.kampbell
   (:require [com.stuartsierra.component :as component]
-            [lang-utils.core :refer [seek ∘]]
+            [lang-utils.core :refer [seek]]
             [kampbell.core :as k]
             [clojure.set :refer [union]]))
 
@@ -10,7 +10,7 @@
     (when equality-specs
       (alter-var-root #'k/equality-specs union equality-specs))
     (when entities
-      (when-let [db (seek (∘ :store val) component)]
+      (when-let [db (seek (comp :store val) component)]
         (k/seed-db (:store (val db)) entities)))
     component)
   (stop [component]
