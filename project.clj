d7bb8fbb7b369343dd0434aeef9992336b53d79d
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2019-12-28 18:55:44 +0200

    Release 0.4.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.danielsz/system "0.4.4-SNAPSHOT"
+(defproject org.danielsz/system "0.4.4"
   :description "Reloaded components à la carte"
   :url "https://github.com/danielsz/system"
   :license {:name "Eclipse Public License"
