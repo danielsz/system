958d37242cfc442d5787a4b029bd5b3fbab11837
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2020-08-31 03:38:36 +0300

    Etsy throttled client

--- src/system/components/etsy.clj
+++ src/system/components/etsy.clj
@@ -2,15 +2,15 @@
   (:require [com.stuartsierra.component :as component]
             [etsy.core :refer [make-client]]))
 
-(defrecord Etsy [token secret]
+(defrecord Etsy [token secret throttled?]
   component/Lifecycle
   (start [component]
-    (let [client (make-client token secret)]
+    (let [client (make-client token secret :throttled? throttled?)]
       (assoc component :client client)))
   (stop [component]
     (assoc component :client nil)))
 
 
 (defn new-etsy-client
-  [token secret]
-  (map->Etsy {:token token :secret secret}))
+  [token secret & {:keys [throttled?] :or {throttled? false}}]
+  (map->Etsy {:token token :secret secret :throttled? throttled?} ))
