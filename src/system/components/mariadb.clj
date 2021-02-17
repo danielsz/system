4c8bf677e9988eec59033b3af4352567c459b50c
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2021-02-17 23:57:19 +0200

    Mariadb

--- src/system/components/mariadb.clj
+++ src/system/components/mariadb.clj
@@ -1,16 +1,17 @@
 (ns system.components.mariadb
   (:require [com.stuartsierra.component :as component])
-  (:import [java.sql DriverManager]))
+  (:import [org.mariadb.jdbc MariaDbDataSource]))
 
-(defrecord MariaDBConnection [jdbc-url]
+(defrecord MariaDB [jdbc-url]
   component/Lifecycle
   (start [component]
-    (let [connection (DriverManager/getConnection jdbc-url )]
-      (assoc component :connection connection)))
+    (let [data-source (MariaDbDataSource. jdbc-url)
+          connection (.getConnection data-source)]
+      (assoc component :connection connection :data-source data-source)))
   (stop [component]
     (when-let [connection (:connection component)]
       (.close connection))
-    (assoc component :connection nil)))
+    (assoc component :connection nil :data-source nil)))
 
-(defn new-mariadb-connection  [& {:keys [jdbc-url]}]
-  (map->MariaDBConnection {:jdbc-url jdbc-url}))
+(defn new-mariadb  [& {:keys [jdbc-url]}]
+  (map->MariaDB {:jdbc-url jdbc-url}))
