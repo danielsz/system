ad1a477c230e290693adec93f0654ec78f0b299a
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2023-08-31 07:22:32 +0300

    Endpoint keyword arguments
    Creates an endpoint. If argument is a vector of route data, will create reitit routes with optional middleware. If argument is a function, it will check for presence of Reitit route protocol and assume that the endpoint is a closure over Reitit routes with component dependencies in scope.

--- src/system/components/endpoint.clj
+++ src/system/components/endpoint.clj
@@ -1,15 +1,24 @@
 (ns system.components.endpoint
-  (:require [com.stuartsierra.component :as component]))
+  (:require [com.stuartsierra.component :as component]
+            [reitit.ring :as ring]
+            [reitit.core :refer [Router]]))
 
-(defrecord Endpoint [f]
+(defrecord Endpoint [routes middleware]
   component/Lifecycle
   (start [component]
-    (assoc component :routes (f component)))
+    (assoc component :routes (cond
+                               (vector? routes) (if (not-empty middleware)
+                                                        (ring/router routes {:reitit.middleware/transform #(into % middleware)})
+                                                        (ring/router routes))
+                               (and (ifn? routes) (satisfies? Router (routes component))) (routes component))))
   (stop [component]
     (dissoc component :routes)))
 
 (defn new-endpoint
-  "Creates an endpoint. An endpoint is a closure over Compojure routes
-  with component dependencies in scope."
-  [routes-fn]
-  (->Endpoint routes-fn))
+  "Creates an endpoint. If argument is a vector of route data, will
+  create reitit routes with optional middleware. If argument is a
+  function, it will check for presence of Reitit route protocol and
+  assume that the endpoint is a closure over Reitit routes with
+  component dependencies in scope."
+  [& {:keys [routes middleware]}]
+  (map->Endpoint {:routes routes :middleware middleware}))
