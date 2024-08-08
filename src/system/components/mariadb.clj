c33ef344178de836700d08070ff0e87a74bf6073
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2024-08-08 15:23:29 +0300

    MariaDB init function

--- src/system/components/mariadb.clj
+++ src/system/components/mariadb.clj
@@ -2,16 +2,17 @@
   (:require [com.stuartsierra.component :as component])
   (:import [org.mariadb.jdbc MariaDbDataSource]))
 
-(defrecord MariaDB [jdbc-url]
+(defrecord MariaDB [jdbc-url init-fn]
   component/Lifecycle
   (start [component]
     (let [data-source (MariaDbDataSource. jdbc-url)
           connection (.getConnection data-source)]
+      (when init-fn (init-fn {:connection connection :data-source data-source}))
       (assoc component :connection connection :data-source data-source)))
   (stop [component]
     (when-let [connection (:connection component)]
       (.close connection))
     (assoc component :connection nil :data-source nil)))
 
-(defn new-mariadb  [& {:keys [jdbc-url]}]
-  (map->MariaDB {:jdbc-url jdbc-url}))
+(defn new-mariadb  [& {:keys [jdbc-url init-fn]}]
+  (map->MariaDB {:jdbc-url jdbc-url :init-fn init-fn}))
