74555399314742a58b946836fa45f616e025e247
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2024-10-05 20:40:20 +0200

    EclipseStore fixes

--- src/system/components/eclipsestore.clj
+++ src/system/components/eclipsestore.clj
@@ -2,23 +2,23 @@
   (:require [com.stuartsierra.component :as component])
   (:import [org.eclipse.store.storage.embedded.types EmbeddedStorage]))
 
-(def storage-manager (EmbeddedStorage/start))
-
 (defrecord EclipseStore []
   component/Lifecycle
   (start [component]
-    (let [data (if (.root storage-manager)
-               (atom (.root storage-manager))
-               (atom {}))]
-      (add-watch data :watcher
+    (let [storage-manager (EmbeddedStorage/start)
+          data (if (.root storage-manager)
+               (.root storage-manager)
+               {})
+          data-in-atom (atom data)]
+      (add-watch data-in-atom :watcher
                (fn [key atom old-state new-state]
                  (.setRoot storage-manager new-state)
                  (.storeRoot storage-manager)))
-      (assoc component :db data)))
+      (assoc component :db data-in-atom :storage-manager storage-manager)))
   (stop [component]
     (remove-watch (:db component) :watcher)
-    (.shutdown storage-manager)
-    (assoc component :db nil)))
+    (.shutdown (:storage-manager component))
+    (dissoc component :db)))
 
 (defn new-eclipsestore [& {:keys []}]
   (map->EclipseStore {}))
