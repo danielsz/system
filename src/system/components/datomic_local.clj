73cc8d43063802653b11cfbf540296038622744b
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2025-07-23 01:52:45 +0800

    Mariadb + Datomic
    - Test datomic storage is valid
    - Mariadb connection pooling

--- src/system/components/datomic_local.clj
+++ src/system/components/datomic_local.clj
@@ -1,10 +1,25 @@
 (ns system.components.datomic-local
   (:require [com.stuartsierra.component :as component]
-            [datomic.client.api :as d]))
+            [clojure.java.io :as io]
+            [datomic.client.api :as d]
+            [clojure.edn :as edn]))
+
+(defn test-storage-directory []
+  (let [conf (io/file (str (System/getProperty "user.home") "/.datomic/local.edn"))]
+    (if (.exists conf)
+      (let [edn (-> conf
+                   slurp
+                   edn/read-string)
+            storage-dir (io/file (:storage-dir edn))]
+        (if (.exists storage-dir)
+          (println "Datomic local:" storage-dir)
+          (throw (ex-info "Storage directory for Datomic local does not exist!" {:value storage-dir}))))
+      (throw (ex-info "Storage directory for Datomic local is not set up!" {:value "~/.datomic/local.edn"})))))
 
 (defrecord DatomicLocal [cfg db init-fn]
   component/Lifecycle
   (start [component]
+    (test-storage-directory)
     (let [client (d/client cfg)
           _ (d/create-database client db)
           conn (d/connect client db)]
--- src/system/components/mariadb.clj
+++ src/system/components/mariadb.clj
@@ -1,18 +1,21 @@
 (ns system.components.mariadb
   (:require [com.stuartsierra.component :as component])
-  (:import [org.mariadb.jdbc MariaDbDataSource]))
+  (:import [org.mariadb.jdbc MariaDbDataSource MariaDbPoolDataSource]))
 
-(defrecord MariaDB [jdbc-url init-fn]
+(defrecord MariaDB [jdbc-url init-fn with-pool? with-connection?]
   component/Lifecycle
   (start [component]
-    (let [data-source (MariaDbDataSource. jdbc-url)
-          connection (.getConnection data-source)]
-      (when init-fn (init-fn {:connection connection :data-source data-source}))
-      (assoc component :connection connection :data-source data-source)))
+    (let [data-source (if with-pool? (MariaDbPoolDataSource. jdbc-url)
+                          (MariaDbDataSource. jdbc-url))]
+      (when init-fn (init-fn {:data-source data-source}))
+      (cond-> component
+        true (assoc :data-source data-source)
+        with-connection? (assoc :connection (.getConnection data-source)))))
   (stop [component]
     (when-let [connection (:connection component)]
       (.close connection))
-    (assoc component :connection nil :data-source nil)))
+    (when with-pool? (.close (:data-source component)))
+    (dissoc component :data-source)))
 
-(defn new-mariadb  [& {:keys [jdbc-url init-fn]}]
-  (map->MariaDB {:jdbc-url jdbc-url :init-fn init-fn}))
+(defn new-mariadb  [& {:keys [jdbc-url init-fn with-pool? with-connection?] :or {with-pool? false with-connection? false} :as options}]
+  (map->MariaDB options))
